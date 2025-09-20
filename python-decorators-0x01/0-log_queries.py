3d270f7a16638100b8780a6bc6ebacf9994d8863
M-Wonder <mybwndr17@gmail.com>
2025-09-20 17:42:47 +0300

    Update 0-log_queries.py

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,1 +1,37 @@
+import sqlite3
+import functools
+
+# decorator to log SQL queries
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # look for the 'query' argument (positional or keyword)
+        query = None
+        if "query" in kwargs:
+            query = kwargs["query"]
+        elif len(args) > 0:
+            query = args[0]
+        
+        if query:
+            print(f"[LOG] Executing SQL Query: {query}")
+        else:
+            print("[LOG] Executing function without explicit query")
+        
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+
+# fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
+print(users)
 
