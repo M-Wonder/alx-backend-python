703d4f39e39012069cadb95a1a90bfad78a05e88
M-Wonder <mybwndr17@gmail.com>
2025-10-06 13:44:24 +0300

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,5 +1,6 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 from .models import Message, Notification, MessageHistory
 
 
@@ -18,10 +19,8 @@ def create_notification(sender, instance, created, **kwargs):
 @receiver(pre_save, sender=Message)
 def log_message_edit(sender, instance, **kwargs):
     """
-    Log the old content of a message before it is edited.
-    Create a MessageHistory record only when content changes.
+    Log old content before editing a message.
     """
-    # Skip if it's a new message (no PK yet)
     if not instance.pk:
         return
 
@@ -30,10 +29,27 @@ def log_message_edit(sender, instance, **kwargs):
     except Message.DoesNotExist:
         return
 
-    # If content has changed, log old content and mark as edited
     if old_message.content != instance.content:
         MessageHistory.objects.create(
             message=instance,
             old_content=old_message.content
         )
         instance.edited = True
+
+
+@receiver(post_delete, sender=User)
+def cleanup_related_data(sender, instance, **kwargs):
+    """
+    Automatically delete all related messages, histories, and notifications
+    when a user deletes their account.
+    """
+    # Delete messages sent or received by the user
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete notifications belonging to the user
+    Notification.objects.filter(user=instance).delete()
+
+    # Delete any orphaned message histories (if messages above are deleted)
+    MessageHistory.objects.filter(message__sender=instance).delete()
+    MessageHistory.objects.filter(message__receiver=instance).delete()
